9f6ff6fbecada784b8d7fa89113c76a60d57963b
Nhu Truong Hoang Phuong <truonghoangphuongnhu@gmail.com>
2021-10-10 22:15:41 +0700

    Build functions to add, update, get all assets

--- application-javascript/routes/addIngredient_Org1.js
+++ application-javascript/routes/addIngredient_Org1.js
@@ -39,17 +39,49 @@ router.get("/", async function (req, res){
 			const network = await gateway.getNetwork(channelName);
 			const contract = network.getContract(chaincodeName);
 
-      console.log('\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger');
+			//Initialize a set of asset data
+			console.log('\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger');
 			await contract.submitTransaction('InitLedger');
 			console.log('*** Result: committed');
 
-      console.log('\n--> Submit Transaction: CreateAsset, creates new asset with id, Name, Type, Owner, Issuer, currentOwner, transferTo arguments');
-			result = await contract.submitTransaction('CreateAsset', 'I23', 'blockchain', 'abc', 'sdfsf', 'Org1', 'sdfsdf', 'sdfsf' );
+			//Create new asset
+			console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, Name, Type, Issuer arguments');
+			result = await contract.submitTransaction('CreateAsset', 'I23', 'Pineapple', 'ingredient','Org1');
 			console.log('*** Result: committed');
 			if (`${result}` !== '') {
 				console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 			}
+
+			//Read asset with given ID
+			console.log('\n--> Evaluate Transaction: ReadAsset, function returns an asset with a given ID');
+			result = await contract.evaluateTransaction('ReadAsset', 'I2');
+			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+
+			//Delete asset with given ID
+			console.log('\n--> Evaluate Transaction: DeleteAsset, function returns an asset with a given ID');
+			result = await contract.evaluateTransaction('DeleteAsset', 'I5');
+			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+
+			//Check for existed asset
+			console.log('\n--> Evaluate Transaction: AssetExists, function returns "true" if an asset with given assetID exist');
+			result = await contract.evaluateTransaction('AssetExists', 'asset1');
+			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+
+			//Update existed asset
+			console.log('\n--> Submit Transaction: UpdateAsset I3, change the Issuer to Org2');
+			await contract.submitTransaction('UpdateAsset', 'I3', 'Orange', 'ingredient', 'Org2');
+			console.log('*** Result: committed');
+
+			//Throw an error when update non-existed asset
+			try {
+				console.log('\n--> Submit Transaction: UpdateAsset I50, I50 does not exist and should return an error');
+				await contract.submitTransaction('UpdateAsset', 'I50', 'Orange', 'ingredient', 'Org2');
+				console.log('******** FAILED to return an error');
+			} catch (error) {
+				console.log(`*** Successfully caught the error: \n ${error}`);
+			}
 		
+			//Return all asset data
 			console.log('\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger');
 			let result = await contract.evaluateTransaction('GetAllAssets');
 			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
